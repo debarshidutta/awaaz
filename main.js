b7c48a8c29ef9e64130802cbf6be2afa58f6ead2
debarshidutta <153851503+debarshidutta@users.noreply.github.com>
2024-09-30 23:55:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings: ['Web Developer','Frontend Developer','Backend Developer'],
+    typeSpeed: 20,
+    backSpeed: 25,
+    backdelay:10,
+    loop:true
+})
